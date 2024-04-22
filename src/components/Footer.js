3e78aa115cd296884e9160df2bdf0ef152bb4024
Tanushree227 <tanushreedas813@gmail.com>
2024-04-22 21:03:47 +0530

    done with footer

---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import logo from '../images/small_logo.png';
+
+export default function Footer() {
+  return (
+    <footer>
+        <section>
+            <div className='company-info'>
+                <img src={logo} alt='' />
+                <p>We are a family owned Mediterranean restaurant, focussed on traditional recipes served with a modern twist.</p>
+            </div>
+            <div>
+                <h3>Important Links</h3>
+                <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/booking'>Reservations</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/menu'>Menu</a></li>
+                    <li><a href='/order'>Order Online</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <ul>
+                    <li>Address: <br />Manipal</li>
+                    <li>Phone: <br />9876543210</li>
+                    <li>Email: <br />littlelemon@gmail.com</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Social Media Links</h3>
+                <ul>
+                    <li><a href='/instagram'>Instagram</a></li>
+                    <li><a href='/facebook'>Facebook</a></li>
+                    <li><a href='/twitter'>Twitter</a></li>
+                    <li><a href='/linkedin'>LinkedIn</a></li>
+                    <li><a href='/threads'>Threads</a></li>
+                </ul>
+            </div>
+        </section>
+    </footer>
+  )
+}
