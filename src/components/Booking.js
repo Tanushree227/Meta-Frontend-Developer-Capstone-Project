ba048ebb9c9a2df41b5296ba466755f50b122d87
Tanushree227 <tanushreedas813@gmail.com>
2024-04-22 20:02:45 +0530

    fixing the props issue

---
+++ src/components/Booking.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import BookingForm from './BookingForm'
+
+export default function Booking(props) {
+  return (
+    <BookingForm availableTime={props.availableTime} dispatch={props.dispatch} SubmitForm={props.SubmitForm} />
+  )
+}
